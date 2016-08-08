a9ae361b76cb81b326a0f14e91eb9de0851e9975
Thrax989 <thrax989@yahoo.com>
2016-08-08 01:36:19 -0700

    Added Phoenix Realm Server

---
+++ MMOCoreORB/bin/scripts/object/custom_content/building/player/sm_hut_house.lua
@@ -0,0 +1,28 @@
+object_building_player_sm_hut_house = object_building_player_shared_sm_hut_house:new {
+	lotSize = 2,
+	baseMaintenanceRate = 16,
+	allowedZones = {"corellia", "dantooine", "lok", "naboo", "rori", "talus", "tatooine", "yavin4", "dathomir", "kaas", "endor"},
+	publicStructure = 1,
+	constructionMarker = "object/building/player/construction/construction_player_house_naboo_small_style_01.iff",
+	length = 1,
+	width = 1,
+	alwaysPublic = 1,
+	abilityRequired = "place_merchant_tent",
+	skillMods = {
+		{"private_medical_rating", 100},
+		{"private_buff_mind", 100},
+		{"private_med_battle_fatigue", 5}
+	},
+	childObjects = {
+			{templateFile = "object/tangible/terminal/terminal_player_structure.iff", x = 2.56417, z = 0.699501, y = 3.14297, ow = -4.37114e-08, ox = 0, oz = 0, oy = 1, containmentType = -1},
+			{templateFile = "object/tangible/sign/player/house_address.iff", x = 1.20, z = 4.60, y = 5.50, ow = 0, ox = 0, oz = 0, oy = 1, cellid = -1, containmentType = -1}
+	},
+	shopSigns = {
+			{templateFile = "object/tangible/sign/player/house_address.iff", x = 1.20, z = 4.60, y = 5.50, ow = 0, ox = 0, oz = 0, oy = 1, cellid = -1, containmentType = -1, requiredSkill = "", suiItem = "@player_structure:house_address"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s01.iff", x = -4.80, z = 0.78, y = 5.55, ow = 1, ox = 0, oz = 0, oy = 0, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_01", suiItem = "@player_structure:shop_sign1"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s02.iff", x = -4.80, z = 0.78, y = 5.55, ow = 1, ox = 0, oz = 0, oy = 0, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_02", suiItem = "@player_structure:shop_sign2"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s03.iff", x = -4.80, z = 0.78, y = 5.55, ow = 1, ox = 0, oz = 0, oy = 0, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_03", suiItem = "@player_structure:shop_sign3"},
+			{templateFile = "object/tangible/sign/player/shop_sign_s04.iff", x = -4.80, z = 0.78, y = 5.55, ow = 1, ox = 0, oz = 0, oy = 0, cellid = -1, containmentType = -1, requiredSkill = "crafting_merchant_management_04", suiItem = "@player_structure:shop_sign4"}		
+	}
+}
+ObjectTemplates:addTemplate(object_building_player_sm_hut_house, "object/building/player/sm_hut_house.iff")
